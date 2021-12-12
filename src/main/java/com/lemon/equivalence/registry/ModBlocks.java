89b02dccaf3ae35c9c66045f7cfe749f224d9dba
Thomas Lord <tlord@parkops.net>
2021-12-12 17:06:53 -0500

    Define required tool using new tag method (breakByTool is deprecated)

--- src/main/java/com/lemon/equivalence/registry/ModBlocks.java
+++ src/main/java/com/lemon/equivalence/registry/ModBlocks.java
@@ -10,10 +10,10 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
-    public static final Block BALANCE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 1).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
-    public static final Block NITRO_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 1).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
-    public static final Block INFERNO_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 1).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
-    public static final Block BLAZING_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 1).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
+    public static final Block BALANCE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
+    public static final Block NITRO_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
+    public static final Block INFERNO_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
+    public static final Block BLAZING_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
 
 public static void registerBlocks(){
     Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "balance_block"), BALANCE_BLOCK);
