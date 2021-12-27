ad32a440cd703f8c3fb03ac9e8eee1c5bdd66588
Thomas Lord <tlord@parkops.net>
2021-12-27 09:19:34 -0500

    Fix formatting

--- src/main/java/com/lemon/equivalence/registry/ModBlocks.java
+++ src/main/java/com/lemon/equivalence/registry/ModBlocks.java
@@ -15,10 +15,10 @@ public class ModBlocks {
     public static final Block INFERNO_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
     public static final Block BLAZING_COAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 20.0f).sounds(BlockSoundGroup.STONE));
 
-public static void registerBlocks(){
-    Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "balance_block"), BALANCE_BLOCK);
-    Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "nitro_coal_block"), NITRO_COAL_BLOCK);
-    Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "inferno_coal_block"), INFERNO_COAL_BLOCK);
-    Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "blazing_coal_block"), BLAZING_COAL_BLOCK);
+    public static void registerBlocks(){
+        Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "balance_block"), BALANCE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "nitro_coal_block"), NITRO_COAL_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "inferno_coal_block"), INFERNO_COAL_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Equivalence.MOD_ID, "blazing_coal_block"), BLAZING_COAL_BLOCK);
     }
 }
